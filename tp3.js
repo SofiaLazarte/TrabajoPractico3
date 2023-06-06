e448ca34fdb929b87723c9a43773a68103da7af1
Sofia Lazarte <sofi.lazarte.sl@gmail.com>
2023-06-06 17:36:27 -0300

    punto dos

--- tp3.js
+++ tp3.js
@@ -1,7 +1,41 @@
 //punto uno
-
+/*
 let meses = ["", "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
 
-let listameses = meses.join ("<br>"+"•" )
+let listameses = meses.join ("<br>"+"•" );
 
-document.write( "Lista de Meses"+ "<br>"+ listameses )
+document.write( "Lista de Meses"+ "<br>"+ listameses );
+*/
+
+//punto dos
+
+let arrayCiudad = [];
+do 
+{
+    let ciudad = prompt("Escriba una ciudad")
+    arrayCiudad.push(ciudad); 
+    
+
+} 
+while (confirm ("Quiere seguir ingresando?") == true);
+
+
+
+document.write("El arreglo tiene " + arrayCiudad.length + " elementos" + "<br> <br>");
+
+document.write("Elemento 1er posicion: " + arrayCiudad[0] + "<br>");
+
+document.write("Elemento 3ra posicion: " + arrayCiudad[2] + "<br>");
+
+
+document.write("Elemento ultima posicion: " + arrayCiudad[arrayCiudad.length - 1] + "<br>");
+
+arrayCiudad.push("Paris");
+
+document.write("Elemento ultima posicion: " + arrayCiudad[arrayCiudad.length - 1] + "<br> ");
+
+document.write("Elemento 2ra posicion: " + arrayCiudad[1] + "<br> <br>");
+
+let lista = arrayCiudad.join("<br>"+"•" );
+
+document.write("Arreglo de Ciudades" + "<br>"  +"•"+ lista);

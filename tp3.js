b174e3993800fa65299faa4f5d2e614705a71cf8
Sofia Lazarte <sofi.lazarte.sl@gmail.com>
2023-06-06 11:36:53 -0300

    punto uno

---
+++ tp3.js
@@ -0,0 +1,7 @@
+//punto uno
+
+let meses = ["", "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+let listameses = meses.join ("<br>"+"•" )
+
+document.write( "Lista de Meses"+ "<br>"+ listameses )
